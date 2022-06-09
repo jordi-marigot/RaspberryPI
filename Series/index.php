d52016e38ddb69143591cefd29960dc8c8398d95
jordi-marigot <98736519+jordi-marigot@users.noreply.github.com>
2022-06-09 02:56:07 +0200

    Add files via upload

---
+++ Series/index.php
@@ -0,0 +1,53 @@
+<?php
+	include '../Login/val.h';
+	session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/util.css">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+<!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+<!--===============================================================================================-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Varela+Round&display=swap" rel="stylesheet">
+
+    <title>HOLA</title>
+</head>
+<body>
+  <div class="limiter">
+    <div class="container-login100">
+      <div class="wrap-login200 p-t-30 p-b-50">
+        <h1 class="hometitle" style="padding-bottom: 30px;margin-top: -320px;">ONFEX <span style="color: #9932CC">></span></h1>
+        <p class="homesubtitle"><em></em></p>
+               <h2 class="homecategory">Service / Webs</h5>
+                 <div class="row" style=" margin-top: 60px">
+                    <div class="col">
+                      <form action ="..\OnTUNE\index.php">
+                         <button class="button-47" role="button"><img src="images\icons\icons8-cloud-64.png" /></button>
+                     	</form>
+                    </div>
+                    <div class="col">
+                      <form action ="..\OnTUNE\index.php">
+                         <button class="button-47" role="button" ><img src="images\icons\icons8-music-64.png" /></button>
+                      </form>
+                    </div>
+                    <div class="col">
+                      <form action ="https://onfex.duckdns.org/nextcloud">
+                         <button class="button-47" role="button" ><img src="images\icons\icons8-netflix-desktop-app-64.png" /></button>
+                      </form>
+                    </div>
+                </div>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
