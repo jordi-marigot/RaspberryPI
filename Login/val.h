59b63c66d00be0b50680eb7c12b728bfb2a620f2
jordi-marigot <98736519+jordi-marigot@users.noreply.github.com>
2022-06-09 02:55:12 +0200

    Delete Login directory

--- Login/val.h
+++
@@ -1,6 +0,0 @@
-<?php
-	session_start();
-		if ((!isset($_SESSION['validat']))||($_SESSION['validat']!=1)) {
-			header('location: ../home.html');
-
-		}
--- Login/val.php
+++
@@ -1,5 +0,0 @@
-<?php
-	session_start();
-	$_SESSION['validat']=1;
-  header('Location: ../home.html')
-?>
